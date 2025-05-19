e4242a80e0778cadfc2457de255d20d2b601da3e
CanFam23 <nickclouse03@gmail.com>
2025-05-18 21:54:58 -0600

    Rename header to navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,34 @@
+import './Navbar.css'
+
+function Navbar(){
+
+    return (
+        <header id="nav-header">
+            <nav id="nav-bar">
+                <span id="logo">
+                    <a href="">NC</a>
+                </span>
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <hr className="nav-line"/>
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <hr className="nav-line"/>
+                        <a className="nav-link" href="#">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <hr className="nav-line"/>
+                        <a className="nav-link" href="#">Experience</a>
+                    </li>
+                    <li className="nav-item">
+                        <hr className="nav-line"/>
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
