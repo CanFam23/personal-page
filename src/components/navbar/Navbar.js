65dcaa801a18f2ab01dfadeac70bedd64b9a90c5
CanFam23 <nickclouse03@gmail.com>
2025-05-20 10:23:11 -0600

    Update mobile view, responsiveness. Add background grid

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,34 +1,46 @@
-import './Navbar.css'
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar(){
 
-    return (
-        <header id="nav-header">
-            <nav id="nav-bar">
-                <span id="logo">
-                    <a href="">NC</a>
-                </span>
-                <ul className="nav-list">
-                    <li className="nav-item">
-                        <hr className="nav-line"/>
-                        <a className="nav-link" href="#">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <hr className="nav-line"/>
-                        <a className="nav-link" href="#">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <hr className="nav-line"/>
-                        <a className="nav-link" href="#">Experience</a>
-                    </li>
-                    <li className="nav-item">
-                        <hr className="nav-line"/>
-                        <a className="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+function Navbar() {
+  return (
+    <header id="nav-header">
+      <nav id="nav-bar">
+        <span id="logo">
+          <a href="">NC</a>
+        </span>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <hr className="nav-line" />
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <hr className="nav-line" />
+            <a className="nav-link" href="#">
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <hr className="nav-line" />
+            <a className="nav-link" href="#">
+              Experience
+            </a>
+          </li>
+          <li className="nav-item">
+            <hr className="nav-line" />
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <FontAwesomeIcon id="menu" icon={faBars} size="2xl" style={{ color: "#ffffff" }} />
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
