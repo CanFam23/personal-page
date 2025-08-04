3b53ca7fb39355b3b911d04a65c372cc2be31360
CanFam23 <nickclouse03@gmail.com>
2025-08-03 22:33:27 -0600

    Fix semantics of project image, add hint to enlarge image

--- src/components/imageModel/ImageModal.js
+++ src/components/imageModel/ImageModal.js
@@ -1,45 +1,47 @@
-import "./ImageModal.css"
+import "./ImageModal.css";
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
-function ImageModal({thumbnailSrc, alt}){
-    // Keep track of if modal is open
-    const [isOpen, setIsOpen] = useState(false);
+function ImageModal({ thumbnailSrc, alt }) {
+  // Keep track of if modal is open
+  const [isOpen, setIsOpen] = useState(false);
 
-    // Used to check if 10% of the base image is in the viewport
-    const {ref, inView} = useInView({threshold: 0.1});
+  // Used to check if 10% of the base image is in the viewport
+  const { ref, inView } = useInView({ threshold: 0.1 });
 
-    // Prevents modal from auto popping back up if it goes off viewport then back on
-    useEffect(() => {
-        if (!inView && isOpen){
-            setIsOpen(false);
-        }
-    }, [inView, isOpen]) // Method is called whenever inView is updated
+  // Prevents modal from auto popping back up if it goes off viewport then back on
+  useEffect(() => {
+    if (!inView && isOpen) {
+      setIsOpen(false);
+    }
+  }, [inView, isOpen]); // Method is called whenever inView is updated
 
-    return (
-        <>
-            <img
-                ref={ref}
-                src={thumbnailSrc}
-                alt={alt}
-                className="thumbnail"
-                onClick={() => setIsOpen(true)}
-            />
+  return (
+    <>
+      <figure>
+        <img
+          ref={ref}
+          src={thumbnailSrc}
+          alt={alt}
+          className="thumbnail"
+          onClick={() => setIsOpen(true)}
+        />
+        <figcaption className="image-hint">Click image to enlarge</figcaption>
+      </figure>
 
-            {isOpen && (
-                <div className="full-screen-overlay"
-                onClick={() => setIsOpen(false)}
-                >
-                    <div className="modal-overlay" onClick={(e) => e.stopPropagation()}>
-                        <button className="close-button" onClick={() => setIsOpen(false)}>×</button>
-                        <img src={thumbnailSrc} alt={alt} className="modal-image"/>
-                    </div>
-                </div>
-            )}
-        </>
-    );
-};
-
+      {isOpen && (
+        <div className="full-screen-overlay" onClick={() => setIsOpen(false)}>
+          <div className="modal-overlay" onClick={(e) => e.stopPropagation()}>
+            <button className="close-button" onClick={() => setIsOpen(false)}>
+              ×
+            </button>
+            <img src={thumbnailSrc} alt={alt} className="modal-image" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
 export default ImageModal;
